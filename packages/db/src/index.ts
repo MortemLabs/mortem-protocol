d3011a9232738578525164021a65be27f60500ef
ifeelsam <sanskarsharma9005@gmail.com>
2026-04-19 18:36:36 +0530

    chore: scaffold prisma package

---
+++ packages/db/src/index.ts
@@ -0,0 +1,3 @@
+// The db package exports Mortem's Prisma singleton and generated Prisma namespace. Services
+// import from here instead of constructing their own clients.
+export * from "./client.js"
